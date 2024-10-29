a218b0ca7f20fb92ca863f38d86695b710f66e96
xieminghe1 <minghe.xie@sophgo.com>
2024-10-29 11:46:07 +0800

    add mlir compile file

---
+++ conver_fp16_3.4.sh
@@ -0,0 +1,16 @@
+model_deploy.py \
+    --mlir new51200_sim1_0321.mlir  \
+    --quantize F16 \
+    --chip mars3 \
+    --model yana_qat_51200_bm1688_f16_sym.bmodel \
+    --num_core 1 \
+    --opt 2 \
+    --quant_input \
+    --quant_output \
+    # --customization_format BGGR_RAW \
+    # --quant_input_list 2,3 \
+    # --fuse_preprocess \
+    # --addr_mode io_tag \
+    # --compress_mode all \
+  
+    # --test_input  new51200_sim1_0321_in_f32.npz \
---
+++ transform3.4.sh
@@ -0,0 +1,8 @@
+model_transform.py \
+    --model_name new51200_sim1_0321 \
+    --model_def ../reshape_test_down.onnx \
+    --keep_aspect_ratio \
+    --input_shapes [[90,4,96,144],[1,16,48,72],[1,1,48,72],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,4,8,8],[1,1,1,1],[1,1,1,1]]  \
+    --mlir new51200_sim1_0321.mlir  \
+    # --test_input /workspace/yana_fp16_51200/cali/input_1.npz \
+    # --test_result new51200_sim1_0321_top_output.npz 

19f2c90fddad31de66cd4a8e83762e3e653800b9
xieminghe1 <141820649+xieminghe1@users.noreply.github.com>
2024-11-13 19:59:15 +0800

    Update transform3.4.sh

--- transform3.4.sh
+++ transform3.4.sh
@@ -3,6 +3,9 @@ model_transform.py \
     --model_def ../reshape_test_down.onnx \
     --keep_aspect_ratio \
     --input_shapes [[90,4,96,144],[1,16,48,72],[1,1,48,72],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,4,8,8],[1,1,1,1],[1,1,1,1]]  \
-    --mlir new51200_sim1_0321.mlir  \
+    --pixel_format bggr \
+    --preprocess_list 1 \
+    --add_postprocess bnr \
+    --mlir new51200_sim1_0321.mlir
     # --test_input /workspace/yana_fp16_51200/cali/input_1.npz \
     # --test_result new51200_sim1_0321_top_output.npz 

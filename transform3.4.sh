f719d8cafca005f636b5623e57fd65844a84bcec
xieminghe1 <141820649+xieminghe1@users.noreply.github.com>
2024-11-13 20:03:34 +0800

    Update transform3.4.sh

--- transform3.4.sh
+++ transform3.4.sh
@@ -2,7 +2,7 @@ model_transform.py \
     --model_name new51200_sim1_0321 \
     --model_def ../reshape_test_down.onnx \
     --keep_aspect_ratio \
-    --input_shapes [[90,4,96,144],[1,16,48,72],[1,1,48,72],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,4,8,8],[1,1,1,1],[1,1,1,1]]  \
+    --input_shapes [[1,4,720,1280],[1,16,360,640],[1,1,360,640],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,4,8,8],[1,1,1,1],[1,1,1,1]]  \
     --pixel_format bggr \
     --preprocess_list 1 \
     --add_postprocess bnr \

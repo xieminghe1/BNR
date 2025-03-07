07b55cd30a476227223956452e8295837d79964d
xieminghe1 <141820649+xieminghe1@users.noreply.github.com>
2025-03-07 10:58:18 +0800

    Update conver_fp16_3.4.sh

--- conver_fp16_3.4.sh
+++ conver_fp16_3.4.sh
@@ -1,16 +1,1 @@
-model_deploy.py \
-    --mlir new51200_sim1_0321.mlir  \
-    --quantize F16 \
-    --chip bm1688 \
-    --model yana_qat_51200_bm1688_f16_sym.bmodel \
-    --num_core 1 \
-    --opt 2 \
-    --quant_input \
-    --quant_output \
-    --customization_format BGGR_RAW \
-    --quant_input_list 2,3 \
-    --fuse_preprocess \
-    --addr_mode io_tag \
-    # --compress_mode all \
-  
-    # --test_input  new51200_sim1_0321_in_f32.npz \
+
